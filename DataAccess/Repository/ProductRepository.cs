7a1c0d7cc973218b7862b5cc2bc9364b83dda2b0
Guidotss <guidoolguin@hotmail.com>
2023-09-20 19:42:15 -0300

    Implementando ProductRepository y UnitOfWork

---
+++ DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.Data;
+using DataAccess.Repository.IRepository;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db; 
+        
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db; 
+        }
+
+        public void Update(Product product)
+        {
+            var productFromDb = _db.Product.FirstOrDefault(prod => prod.Id == product.Id); 
+            if(productFromDb != null)
+            {
+                productFromDb.Name = product.Name;
+                productFromDb.Description = product.Description;
+                productFromDb.Price = product.Price;
+                productFromDb.Quantity = product.Quantity;
+
+                _db.SaveChanges(); 
+            }
+        }
+    }
+}
---
+++ DataAccess/Repository/UnitOfWork1.cs
@@ -0,0 +1,32 @@
+﻿using DataAccess.Data;
+using DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public IProductRepository Product { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Product = new ProductRepository(_db); 
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose(); 
+        }
+
+        public async Task Save()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
